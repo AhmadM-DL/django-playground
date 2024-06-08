82a428da2971534e08edf73d57d2366b921ce9e2
ElsabbahInstitute <ahmad.m.mustapha@hotmail.com>
2024-06-08 20:36:51 +0300

    changes

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-
-# Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=200, unique=True)
     location = models.CharField(max_length=200)
